bfef58ff32bfaf1e36434172213deecfb6765c92
Sergej Schefer <sergej.schefer@web.de>
2019-11-19 07:08:34 +0100

    setting up renderer

--- source/renderer.c
+++ source/renderer.c
@@ -2,10 +2,39 @@
 #include <assert.h>
 #include <GL/glew.h>
 
+void init_shaders(Renderer* renderer)
+{
+    
+}
 
-void load_shaders(Renderer* renderer)
+void destroy_shaders(Renderer* renderer)
+{
+    if (renderer->_sprite_shader._vertex_shader._id)
+    {
+        glDeleteShader(renderer->_sprite_shader._vertex_shader._id);
+        renderer->_sprite_shader._vertex_shader._id = 0;
+    }
+
+    if (renderer->_sprite_shader._fragment_shader._id)
+    {
+        glDeleteShader(renderer->_sprite_shader._fragment_shader._id);    
+        renderer->_sprite_shader._fragment_shader._id = 0;
+    }
+}
+
+void init_render_target(Renderer* renderer)
 {
 
+}
+
+void set_gl_state_pre_render()
+{
+    // set culling & stuff
+}
+
+void set_gl_state_post_render()
+{
+    // unset culling & stuff
 }
 
 /********************************************************
@@ -14,8 +43,12 @@ void load_shaders(Renderer* renderer)
 
 Renderer create_renderer(RendererDescription renderer_description)
 {
-    Renderer renderer = { 0 };
-    load_shaders(&renderer);
+    Renderer renderer =
+    {
+        ._render_width = renderer_description._render_width,
+        ._render_height = renderer_description._render_height
+    };
+
     return renderer;
 }
 
@@ -23,21 +56,66 @@ void initialize_renderer(Renderer* renderer)
 {
     glewExperimental = GL_TRUE;
     assert(glewInit() == GLEW_OK);
+
+    init_shaders(renderer);
+    init_render_target(renderer);
 }
 
 void deinitialize_renderer(Renderer* renderer)
 {
+    destroy_texture(renderer->_render_target);
+    destroy_shaders(renderer);
+}
 
+void resize(Renderer* renderer, uint64 window_width, uint64 window_height)
+{
+    // todo: resize based on aspect ratio
 }
 
 void render(Renderer* renderer, Sprite* sprites, uint64 sprites_count)
 {
-    glClearColor(1.0f, 0.3f, 1.0f, 1.0f);
+    set_gl_state_pre_render();
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    set_gl_state_post_render();
 }
 
 TextureId create_texture(TextureDescription texture_description, void* data)
 {
     TextureId texture_id = { 0 };
     return texture_id;
+}
+
+void destroy_texture(TextureId texture)
+{
+    glDeleteTextures(1, (GLuint*) &texture._id);
+} 
+
+Sprite create_sprite(TextureId texture)
+{
+    Sprite sprite =
+    {
+        ._texture = texture,
+        ._transform =
+        { 
+            ._position[0] = 0.0f,
+            ._position[1] = 0.0f,
+            ._position[2] = 0.0f,
+
+            ._scale[0] = 1.0f,
+            ._scale[1] = 1.0f,
+
+            ._rotation = 0.0f,
+
+            ._uv_scale[0] = 1.0f,
+            ._uv_scale[1] = 1.0f,
+
+            ._uv_offset[0] = 0.0f,
+            ._uv_offset[1] = 0.0f,
+        }
+    };
+
+    return sprite;
 }
--- source/renderer.h
+++ source/renderer.h
@@ -3,7 +3,7 @@
 
 typedef struct Transform
 {
-    float _position[2];
+    float _position[3];
     float _scale[2];
     float _rotation;
     float _uv_scale[2];
@@ -33,24 +33,37 @@ typedef struct TextureId
 
 typedef struct Sprite
 {
-    TextureId _texture_id;
+    TextureId _texture;
     Transform _transform;
 } Sprite;
 
 typedef struct RendererDescription
 {
-    uint64 _id;
+    uint64 _render_width;
+    uint64 _render_height;
+    uint64 _window_width;
+    uint64 _window_height;
 } RendererDescription;
 
 typedef struct Renderer
 {
-    uint64 _id;
+    uint64 _render_width;
+    uint64 _render_height;
+
     Shader _sprite_shader;
+    Shader _to_screen_shader;
+
+    TextureId _render_target;
 } Renderer;
 
 Renderer create_renderer(RendererDescription renderer_description);
 void initialize_renderer(Renderer* renderer);
 void deinitialize_renderer(Renderer* renderer);
+void resize(Renderer* renderer, uint64 window_width, uint64 window_height);
 
 void render(Renderer* renderer, Sprite* sprites, uint64 sprites_count);
+
 TextureId create_texture(TextureDescription texture_description, void* data);
+void destroy_texture(TextureId texture);
+
+Sprite create_sprite(TextureId texture);
